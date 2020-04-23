248cc4df88c719af3cb00b410aa04a9836c1eb0c
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-04-23 15:45:09 +0900

    WorkモデルとSeeder作成

---
+++ database/factories/WorkFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Work;
+use Faker\Generator as Faker;
+
+$factory->define(Work::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence,
+        'type' => $faker->numberBetween(0,1),
+        'category' => $faker->numberBetween(1,8),
+        'max_price' => $faker->numberBetween(30000, 50000),
+        'min_price' => $faker->numberBetween(5000, 10000),
+        'content' => $faker->text,
+        'user_id' => function() {
+            return factory(App\User::class)->create()->id;
+        },
+        'is_closed' => 0,
+    ];
+});
