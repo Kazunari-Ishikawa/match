a9e438cb3c0b6e990a34693c36bbf21e95267af5
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-02 16:36:29 +0900

    migrate/応募テーブルの作成

---
+++ database/migrations/2020_05_02_163233_create_applies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('work_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('work_id')->references('id')->on('works');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applies');
+    }
+}
