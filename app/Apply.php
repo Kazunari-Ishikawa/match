3cd5b6eef71f1d7cd14b24fd8f5a21c6960201a6
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-02 16:39:21 +0900

    model/Applyモデル作成

---
+++ app/Apply.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Apply extends Model
+{
+    public function work()
+    {
+        return $this->belongsTo('App\Work');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+}
