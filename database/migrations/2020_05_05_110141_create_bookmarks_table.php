a362e00d3a67a987752ceb1fee88177b33579cbc
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-05 11:09:10 +0900

    migrate/bookmarksテーブル作成

---
+++ database/migrations/2020_05_05_110141_create_bookmarks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('work_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('work_id')->references('id')->on('works');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+}
