93710e44e00133eda932920a3999c4b68d17199e
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-06 11:46:05 +0900

    comments.index/コメントのデフォルトアイコン設定

--- resources/js/components/Comment.vue
+++ resources/js/components/Comment.vue
@@ -6,7 +6,11 @@
     </div>
     <div class="c-comment__body">
       <div class="c-comment__icon">
-        <img src alt class="c-comment__img" />
+        <img
+          :src="comment.user.icon ? comment.user.icon :'/images/18445.png'"
+          alt
+          class="c-comment__img"
+        />
         <div v-show="comment.user_id === comment.work.user_id" class="c-comment__tag">依頼者</div>
       </div>
       <div class="c-comment__main">
