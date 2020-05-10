667941a29291949c1372f8ab33a1a7a63243c5f1
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-10 13:37:58 +0900

    サイドバー文言修正

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,8 +1,8 @@
 <section class="l-sidebar">
   <p class="c-menu__tab">依頼者向け</p>
   <ul class="c-menu--side">
-    <li class="c-menu__item c-menu__item--side"><a href="{{ route('works.new') }}" class="c-menu__link c-menu__link--side">案件を登録する</a></li>
-    <li class="c-menu__item c-menu__item--side"><a href="{{ route('works.registered') }}" class="c-menu__link c-menu__link--side">登録した案件</a></li>
+    <li class="c-menu__item c-menu__item--side"><a href="{{ route('works.new') }}" class="c-menu__link c-menu__link--side">依頼する</a></li>
+    <li class="c-menu__item c-menu__item--side"><a href="{{ route('works.registered') }}" class="c-menu__link c-menu__link--side">依頼した案件</a></li>
     <li class="c-menu__item c-menu__item--side"><a href="{{ route('works.closed') }}" class="c-menu__link c-menu__link--side">成約済み案件</a></li>
     <li class="c-menu__item c-menu__item--side"><a href="{{ route('comments.index')}}" class="c-menu__link c-menu__link--side">コメントした案件</a></li>
     <li class="c-menu__item c-menu__item--side"><a href="{{ route('works.bookmarks') }}" class="c-menu__link c-menu__link--side">気になるリスト</a></li>
