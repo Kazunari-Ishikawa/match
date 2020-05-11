5d650a76b5e83430ab8d589737af4d98238fb90f
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-11 12:05:57 +0900

    works.search/serach.vue追加、検索API作成

--- resources/js/components/IndexWorks.vue
+++ resources/js/components/IndexWorks.vue
@@ -1,84 +1,7 @@
 <template>
   <div class="l-container l-container--withSide">
     <!-- サイドバー -->
-    <section class="l-sidebar">
-      <form action class="c-form--side" @submit.prevent="searchWorks">
-        <div class="c-form__group">
-          <p class="c-form__label">案件種別</p>
-          <label for="type1" class="c-form__label--radio">
-            <input
-              type="radio"
-              name="type"
-              id="type1"
-              value="1"
-              class="c-form__radio"
-              v-model="form.type"
-            />単発
-          </label>
-          <label for="type2" class="c-form__label--radio">
-            <input
-              type="radio"
-              name="type"
-              id="type2"
-              value="2"
-              class="c-form__radio"
-              v-model="form.type"
-            />レベニューシェア
-          </label>
-        </div>
-        <div class="c-form__group">
-          <label for="category" class="c-form__label">カテゴリ</label>
-          <select name="category" id="category" class="c-form__select" v-model="form.category">
-            <option value="0">指定しない</option>
-            <option value="1">ホームページ制作</option>
-            <option value="2">WEBシステム開発</option>
-            <option value="3">業務システム開発</option>
-            <option value="4">アプリ開発</option>
-            <option value="5">ECサイト構築</option>
-            <option value="6">サーバー・クラウド</option>
-            <option value="7">WEBマーケティング</option>
-          </select>
-        </div>
-        <div class="c-form__group">
-          <p class="c-form__label">金額</p>
-          <select
-            name="lower"
-            id="lower"
-            class="c-form__select c-form__select--price"
-            v-model="form.minPrice"
-          >
-            <option value="0">指定しない</option>
-            <option value="1">1,000</option>
-            <option value="2">3,000</option>
-            <option value="3">5,000</option>
-            <option value="4">10,000</option>
-            <option value="5">50,000</option>
-            <option value="6">100,000</option>
-            <option value="7">500,000</option>
-            <option value="8">1,000,000</option>
-          </select>
-          <p class="c-form__label--back">円以上〜</p>
-          <select
-            name="upper"
-            id="uppper"
-            class="c-form__select c-form__select--price"
-            v-model="form.maxPrice"
-          >
-            <option value="0">指定しない</option>
-            <option value="1">1,000</option>
-            <option value="2">3,000</option>
-            <option value="3">5,000</option>
-            <option value="4">10,000</option>
-            <option value="5">50,000</option>
-            <option value="6">100,000</option>
-            <option value="7">500,000</option>
-            <option value="8">1,000,000</option>
-          </select>
-          <p class="c-form__label--back">円以下</p>
-        </div>
-        <input type="submit" class="c-btn c-btn--full" value="絞り込み" />
-      </form>
-    </section>
+    <SearchComponent @click-search="searchWorks" />
 
     <!-- メインコンテンツ -->
     <section class="l-container__body--withSide">
@@ -101,12 +24,14 @@
 </template>
 
 <script>
+import SearchComponent from "./SearchComponent";
 import Work from "./Work";
 import Loader from "./Loader";
 import Pagination from "./Pagination";
 
 export default {
   components: {
+    SearchComponent,
     Work,
     Loader,
     Pagination
@@ -139,10 +64,10 @@ export default {
       this.lastPage = response.data.last_page;
       this.isLoading = false;
     },
-    async searchWorks() {
-      console.log(this.form);
+    async searchWorks(form) {
+      console.log(form);
       const response = await axios.post("/api/works/search", {
-        form: this.form
+        form
       });
       console.log(response);
     },
---
+++ resources/js/components/SearchComponent.vue
@@ -0,0 +1,101 @@
+<template>
+  <!-- サイドバー -->
+  <section class="l-sidebar">
+    <form action class="c-form--side" @submit.prevent="clickSearch">
+      <div class="c-form__group">
+        <p class="c-form__label">案件種別</p>
+        <label for="type1" class="c-form__label--radio">
+          <input
+            type="radio"
+            name="type"
+            id="type1"
+            value="0"
+            class="c-form__radio"
+            v-model="form.type"
+          />単発
+        </label>
+        <label for="type2" class="c-form__label--radio">
+          <input
+            type="radio"
+            name="type"
+            id="type2"
+            value="1"
+            class="c-form__radio"
+            v-model="form.type"
+          />レベニューシェア
+        </label>
+      </div>
+      <div class="c-form__group">
+        <label for="category" class="c-form__label">カテゴリ</label>
+        <select name="category" id="category" class="c-form__select" v-model="form.category">
+          <option value="0">指定しない</option>
+          <option value="1">ホームページ制作</option>
+          <option value="2">WEBシステム開発</option>
+          <option value="3">業務システム開発</option>
+          <option value="4">アプリ開発</option>
+          <option value="5">ECサイト構築</option>
+          <option value="6">サーバー・クラウド</option>
+          <option value="7">WEBマーケティング</option>
+        </select>
+      </div>
+      <div class="c-form__group">
+        <p class="c-form__label">金額</p>
+        <select
+          name="lower"
+          id="lower"
+          class="c-form__select c-form__select--price"
+          v-model="form.minPrice"
+        >
+          <option value="0">指定しない</option>
+          <option value="1">1,000</option>
+          <option value="2">3,000</option>
+          <option value="3">5,000</option>
+          <option value="4">10,000</option>
+          <option value="5">50,000</option>
+          <option value="6">100,000</option>
+          <option value="7">500,000</option>
+          <option value="8">1,000,000</option>
+        </select>
+        <p class="c-form__label--back">円以上〜</p>
+        <select
+          name="upper"
+          id="uppper"
+          class="c-form__select c-form__select--price"
+          v-model="form.maxPrice"
+        >
+          <option value="0">指定しない</option>
+          <option value="1">1,000</option>
+          <option value="2">3,000</option>
+          <option value="3">5,000</option>
+          <option value="4">10,000</option>
+          <option value="5">50,000</option>
+          <option value="6">100,000</option>
+          <option value="7">500,000</option>
+          <option value="8">1,000,000</option>
+        </select>
+        <p class="c-form__label--back">円以下</p>
+      </div>
+      <input type="submit" class="c-btn c-btn--full" value="絞り込み" />
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        type: 0,
+        category: 0,
+        minPrice: 0,
+        maxPrice: 0
+      }
+    };
+  },
+  methods: {
+    clickSearch() {
+      this.$emit("click-search", this.form);
+    }
+  }
+};
+</script>
