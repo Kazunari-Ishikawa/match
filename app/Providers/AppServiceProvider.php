acaf4720c45a1b54c00ac06f4bb1299e46425763
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-16 19:51:03 +0900

    HTTPSに対応させるように

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
         // 商用環境以外でSQLログを出力する
         if (config('app.env') !== ('production')) {
             \DB::listen(function($query) {
-                // \Log::debug("Query Time:{$query->time}s], $query->sql");
+                \Log::debug("Query Time:{$query->time}s], $query->sql");
             });
         }
     }
@@ -28,6 +29,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
