de07a0c8696d41d08107b4c710daa347ce5ef051
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-19 19:55:31 +0900

    MessagesSeeder作成

--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -13,6 +13,35 @@ class MessagesTableSeeder extends Seeder
      */
     public function run()
     {
-        // 
+        // 応募データ作成
+        DB::table('applies')->insert([
+            'work_id' => 1,
+            'user_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        // Board作成
+        DB::table('boards')->insert([
+            'work_id' => 1,
+            'from_user_id' => 1,
+            'to_user_id' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        // Message作成
+        DB::table('messages')->insert([
+            'board_id' => 1,
+            'user_id' => 1,
+            'content' => 'テストテストテスト',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        DB::table('messages')->insert([
+            'board_id' => 1,
+            'user_id' => 2,
+            'content' => 'こんばんは',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
