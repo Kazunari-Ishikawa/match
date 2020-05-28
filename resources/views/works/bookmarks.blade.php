ef9f6a0f20011274bb78897c66d72bfb6d64115a
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-28 15:22:31 +0900

    workとcommentのTDKを設定

--- resources/views/works/bookmarks.blade.php
+++ resources/views/works/bookmarks.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
+@section('description')
+<meta name="description" content="気になる案件一覧ページです。matchworksとは、IT系の仕事探しや仕事を依頼したい方々向けに、かんたん3ステップで繋げるビジネスマッチングサービスです。">
+@endsection
+
+@section('keywords')
+<meta name="keywords" content="気になる, 案件一覧">
+@endsection
+
+@section('title')
+<title>気になる案件 - {{ config('app.name', 'Laravel') }}</title>
+@endsection
+
 @section('content')
 <div class="l-container l-container--withSide">
 
