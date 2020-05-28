a132bd86526afbc8fd4e177d19a77552f153c850
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-05-28 11:57:06 +0900

    404ページをCSSと合わせて作成

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,17 +6,19 @@
 
   <!-- メインコンテンツ -->
   <section class="l-container__body">
+    <div class="c-errorPage">
+      <h2 class="c-errorPage__title">404 Not Found</h2>
+      <p class="c-errorPage__message">該当アドレスのページを見つけることができませんでした。</p>
 
-    <p>存在しないページです。</p>
+      @guest
+      <p class="c-errorPage__link"><a href="/">TOPページへ戻る</a></p>
+      @endguest
 
-    @guest
-    <p class="c-form__link"><a href="/">TOPページへ戻る</a></p>
-    @endguest
+      @auth
+      <p class="c-errorPage__link"><a href="/mypage">マイページへ戻る</a></p>
+      @endauth
 
-    @auth
-    <p class="c-form__link"><a href="/mypage">マイページへ戻る</a></p>
-    @endauth
-
+    </div>
   </section>
 
 </div>
